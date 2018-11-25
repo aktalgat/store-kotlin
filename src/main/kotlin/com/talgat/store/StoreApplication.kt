7835b262a37a09eb51087c8e6ec47d79ce6c71b1
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-25 11:20:36 +0600

    Enable spring security

--- src/main/kotlin/com/talgat/store/StoreApplication.kt
+++ src/main/kotlin/com/talgat/store/StoreApplication.kt
@@ -2,8 +2,10 @@ package com.talgat.store
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 class StoreApplication
 
 fun main(args: Array<String>) {
