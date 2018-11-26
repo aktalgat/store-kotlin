2f3ae24618c8a7534c2a3675bfb871ba4b0b3d59
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-26 17:03:47 +0600

    Remove some unused functions

--- src/main/kotlin/com/talgat/store/data/dao/UserRepository.kt
+++ src/main/kotlin/com/talgat/store/data/dao/UserRepository.kt
@@ -1,20 +1,12 @@
 package com.talgat.store.data.dao
 
 import com.talgat.store.data.model.User
-import org.springframework.stereotype.Repository
 
 interface UserRepository {
-    fun findByEmail(email: String): User?
 
-    fun findByUsernameOrEmail(username: String, email: String): User?
-
-    fun findByIdIn(userIds: List<Long>): List<User>
-
-    fun findByUsername(username: String): User?
-
-    fun existsByUsername(username: String): Boolean
-
-    fun existsByEmail(email: String): Boolean
+    fun findByPhone(phone: String): User?
 
     fun findById(id: Long) : User?
+
+    fun save(user: User)
 }
