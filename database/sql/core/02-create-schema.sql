13cd901005af41d0d6c270325799dfd1ebbe6678
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-26 16:12:31 +0600

    Refactor authority table to roles

--- database/sql/core/02-create-schema.sql
+++ database/sql/core/02-create-schema.sql
@@ -20,21 +20,21 @@ CREATE TABLE core.users(
   CONSTRAINT pk_users PRIMARY KEY(id)
 );
 
-CREATE TABLE core.authority(
+CREATE TABLE core.roles(
   id BIGINT NOT NULL,
   name VARCHAR(20),
 
-  CONSTRAINT pk_authority PRIMARY KEY(id)
+  CONSTRAINT pk_roles PRIMARY KEY(id)
 );
 
-CREATE SEQUENCE core.user_authority_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
+CREATE SEQUENCE core.user_roles_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
 
-CREATE TABLE core.user_authority(
+CREATE TABLE core.user_roles(
   id BIGINT DEFAULT nextval('core.user_authority_seq') NOT NULL,
   user_id BIGINT NOT NULL,
-  authority_id BIGINT NOT NULL,
+  role_id BIGINT NOT NULL,
 
-  CONSTRAINT pk_user_authority PRIMARY KEY(id),
-  CONSTRAINT fk_user_authority_user_id FOREIGN KEY (user_id) REFERENCES core.users(id),
-  CONSTRAINT fk_user_authority_authority_id FOREIGN KEY (authority_id) REFERENCES core.authority(id)
+  CONSTRAINT pk_user_roles PRIMARY KEY(id),
+  CONSTRAINT fk_user_roles_user_id FOREIGN KEY (user_id) REFERENCES core.users(id),
+  CONSTRAINT fk_user_roles_authority_id FOREIGN KEY (role_id) REFERENCES core.roles(id)
 );
