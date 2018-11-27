44a364c583b1da52224dd361b70a1fcef08b03c1
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-27 14:26:30 +0600

    Fix: sql relations

--- database/sql/core/02-create-schema.sql
+++ database/sql/core/02-create-schema.sql
@@ -30,11 +30,11 @@ CREATE TABLE core.roles(
 CREATE SEQUENCE core.user_roles_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
 
 CREATE TABLE core.user_roles(
-  id BIGINT DEFAULT nextval('core.user_authority_seq') NOT NULL,
+  id BIGINT DEFAULT nextval('core.user_roles_seq') NOT NULL,
   user_id BIGINT NOT NULL,
   role_id BIGINT NOT NULL,
 
   CONSTRAINT pk_user_roles PRIMARY KEY(id),
   CONSTRAINT fk_user_roles_user_id FOREIGN KEY (user_id) REFERENCES core.users(id),
-  CONSTRAINT fk_user_roles_authority_id FOREIGN KEY (role_id) REFERENCES core.roles(id)
+  CONSTRAINT fk_user_roles_roles_id FOREIGN KEY (role_id) REFERENCES core.roles(id)
 );
--- database/sql/initial-data/01-initial-data.sql
+++ database/sql/initial-data/01-initial-data.sql
@@ -1,9 +1,9 @@
 --Password encoder https://www.dailycred.com/article/bcrypt-calculator
 --Max number round 10
 --Default password 12345
-INSERT INTO core.users(id, phone, phone, first_name, last_name, password, enabled) VALUES
+INSERT INTO core.users(id, phone, email, first_name, last_name, password, enabled) VALUES
   (1, '+996772136130', 'user@domain.com', 'Александр', 'Иванов', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', true);
-INSERT INTO core.users(id, password, first_name, last_name, email, phone, enabled) VALUES
+INSERT INTO core.users(id, phone, email, first_name, last_name, password, enabled) VALUES
   (2, '+996557136130', 'admin@domain.com', 'Роман', 'Александров', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', true);
 
 INSERT INTO core.roles(id, name) VALUES (1, 'ROLE_USER');
