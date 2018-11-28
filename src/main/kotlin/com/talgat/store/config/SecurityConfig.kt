2e36d105ec9ad1431afae104f1febf1daff640b7
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-28 10:31:43 +0600

    Spring security config

---
+++ src/main/kotlin/com/talgat/store/config/SecurityConfig.kt
@@ -0,0 +1,114 @@
+package com.talgat.store.config
+
+import com.google.common.collect.ImmutableList
+import com.talgat.store.security.CustomUserDetailsService
+import com.talgat.store.security.JwtAuthenticationEntryPoint
+import com.talgat.store.security.JwtAuthenticationFilter
+import com.talgat.store.security.JwtTokenProvider
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.BeanIds
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(private val customUserDetailsService: CustomUserDetailsService,
+                     private val unauthorizedHandler: JwtAuthenticationEntryPoint, private val tokenProvider: JwtTokenProvider) : WebSecurityConfigurerAdapter() {
+    @Value("\${app.origin}")
+    private val origin: String? = null
+
+    @Throws(Exception::class)
+    public override fun configure(authenticationManagerBuilder: AuthenticationManagerBuilder?) {
+        authenticationManagerBuilder!!
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder())
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Throws(Exception::class)
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun jwtAuthenticationFilter(): JwtAuthenticationFilter {
+        return JwtAuthenticationFilter(tokenProvider, customUserDetailsService)
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = ImmutableList.of(origin!!)
+        configuration.allowedMethods = ImmutableList.of("*")
+        configuration.allowCredentials = true
+        configuration.allowedHeaders = ImmutableList.of("*")
+        configuration.exposedHeaders = ImmutableList.of("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Access-Control-Max-Age",
+                "Access-Control-Request-Headers", "Access-Control-Request-Method")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+
+                .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js",
+                        "/**/*.woff",
+                        "/**/*.woff2",
+                        "/**/*.otf",
+                        "/**/*.ttf"
+                )
+                .permitAll()
+                .antMatchers("/api/public/**")
+                .permitAll()
+                .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter::class.java)
+
+    }
+}
