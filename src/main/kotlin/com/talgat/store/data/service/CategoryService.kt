9115e3bdc818badc13fdd472f30c7901498b5757
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-29 12:11:13 +0600

    Category service to work with data layer

---
+++ src/main/kotlin/com/talgat/store/data/service/CategoryService.kt
@@ -0,0 +1,10 @@
+package com.talgat.store.data.service
+
+import com.talgat.store.api.payload.CategoryRequest
+import com.talgat.store.data.model.Category
+
+interface CategoryService {
+    fun getCategoryList(): List<Category>
+    fun saveCategory(categoryRequest: CategoryRequest): Category
+    fun saveCategory(category: Category): Category
+}
---
+++ src/main/kotlin/com/talgat/store/data/service/CategoryServiceImpl.kt
@@ -0,0 +1,39 @@
+package com.talgat.store.data.service
+
+import com.talgat.store.api.payload.CategoryRequest
+import com.talgat.store.data.dao.CategoryRepository
+import com.talgat.store.data.model.Category
+import com.talgat.store.exception.InternalException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class CategoryServiceImpl(val categoryRepository: CategoryRepository) : CategoryService {
+
+    companion object {
+        val log : Logger = LoggerFactory.getLogger(CategoryServiceImpl::class.java)
+    }
+
+    override fun saveCategory(categoryRequest: CategoryRequest): Category {
+        val category = Category(categoryRequest.name)
+
+        return saveCategory(category)
+    }
+
+    override fun saveCategory(category: Category): Category {
+        var newCategory: Category? = null
+        try {
+            newCategory = categoryRepository.save(category)
+        } catch (e: Exception) {
+            throw InternalException("Internal error in saving")
+        }
+
+        log.info("Saved category: {}", newCategory)
+        return newCategory
+    }
+
+    override fun getCategoryList(): List<Category> {
+        return categoryRepository.findAll()
+    }
+}
