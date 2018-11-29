357eaaaa941aeab370bb5dd3a3d246f3ffc37166
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-29 10:23:37 +0600

    Abstract public protected classes

---
+++ src/main/kotlin/com/talgat/store/api/AbstractProtectedApi.kt
@@ -0,0 +1,6 @@
+package com.talgat.store.api
+
+import com.talgat.store.api.annotation.ProtectedApiAnnotation
+
+@ProtectedApiAnnotation
+abstract class AbstractProtectedApi
---
+++ src/main/kotlin/com/talgat/store/api/AbstractPublicApi.kt
@@ -0,0 +1,6 @@
+package com.talgat.store.api
+
+import com.talgat.store.api.annotation.PublicApiAnnotation
+
+@PublicApiAnnotation
+abstract class AbstractPublicApi
