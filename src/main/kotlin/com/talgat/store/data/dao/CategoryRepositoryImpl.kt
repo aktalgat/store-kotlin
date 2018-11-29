567d6f69a2e5ee3a323b7964bb0597daf6f1ec05
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-29 12:10:44 +0600

    Category repository and empty implement

---
+++ src/main/kotlin/com/talgat/store/data/dao/CategoryRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.talgat.store.data.dao
+
+import com.talgat.store.data.model.Category
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+
+@Repository
+@Transactional
+class CategoryRepositoryImpl(val jdbcTemplate: JdbcTemplate) : CategoryRepository {
+    override fun findAll(): List<Category> {
+        TODO("not implemented")
+    }
+
+    override fun findById(id: Long): Category? {
+        TODO("not implemented")
+    }
+
+    override fun save(category: Category): Category {
+        TODO("not implemented")
+    }
+
+}
