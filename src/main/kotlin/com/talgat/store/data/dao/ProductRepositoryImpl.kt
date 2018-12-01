39f5a81b17be75d35fe54fdd2efce07ef30030b1
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-12-01 12:25:46 +0600

    Product repository implement save

--- src/main/kotlin/com/talgat/store/data/dao/ProductRepositoryImpl.kt
+++ src/main/kotlin/com/talgat/store/data/dao/ProductRepositoryImpl.kt
@@ -2,7 +2,9 @@ package com.talgat.store.data.dao
 
 import com.talgat.store.data.model.Product
 import com.talgat.store.data.model.ProductImage
+import com.talgat.store.exception.InternalException
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.support.GeneratedKeyHolder
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,9 +14,23 @@ class ProductRepositoryImpl(private val jdbcTemplate: JdbcTemplate) : ProductRep
     private val query = "SELECT id, category_id, name, description, short_description, additional_info, " +
                     "badge, price, price_old, stars FROM core.products"
     private val imageQuery = "SELECT id, product_id, url FROM core.product_images"
+    private val saveQuery = "INSERT INTO core.products(category_id, name, description, short_description, " +
+            "additional_info, badge, price, price_old, stars) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    private val imageInsertQuery = "INSERT INTO core.product_images(product_id, url) VALUES (?, ?)"
 
     override fun save(product: Product): Product {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val holder = GeneratedKeyHolder()
+        jdbcTemplate.update(saveQuery, arrayOf(product.categoryId, product.name, product.description,
+                product.description, product.shortDescription, product.additionalInfo, product.badge, product.price,
+                product.priceOld, product.stars), holder)
+        val id: Long = holder.key?.toLong() ?: throw InternalException("Not inserted id fo product")
+
+        val list = product.productImageList.map {
+            arrayOf(id, it.url)
+        }
+        jdbcTemplate.batchUpdate(imageInsertQuery, list)
+
+        return product.copy(id = id)
     }
 
     override fun findAll(): List<Product> {
