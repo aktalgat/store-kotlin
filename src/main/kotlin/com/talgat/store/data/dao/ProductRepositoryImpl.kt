1a6bad1f1ab34e6a695b556ffda6455e1e9b16cb
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-12-01 10:50:33 +0600

    Product repository find all implementation

--- src/main/kotlin/com/talgat/store/data/dao/ProductRepositoryImpl.kt
+++ src/main/kotlin/com/talgat/store/data/dao/ProductRepositoryImpl.kt
@@ -8,11 +8,18 @@ import org.springframework.transaction.annotation.Transactional
 @Repository
 @Transactional
 class ProductRepositoryImpl(private val jdbcTemplate: JdbcTemplate) : ProductRepository {
+    private val query = "SELECT id, category_id, name, description, short_description, additional_info, " +
+                    "badge, price, price_old, stars FROM core.products"
+
     override fun save(product: Product): Product {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun findAll(): List<Product> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return jdbcTemplate.query(query) { rs, _ ->
+                    Product(rs.getLong("id"), rs.getLong("category_id"), rs.getString("name"),
+                    rs.getString("description"), rs.getString("short_description"), rs.getString("additional_info"),
+                    rs.getString("badge"), rs.getDouble("price"), rs.getDouble("price_old"), rs.getInt("stars"), listOf())
+                }
     }
 }
