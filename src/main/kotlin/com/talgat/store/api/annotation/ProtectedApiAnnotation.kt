2b4f29804fb6740baac14ec9fdb4a54dbbcb62b2
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-02-21 15:55:37 +0600

    Remove unused RestController annotation in general annotations

--- src/main/kotlin/com/talgat/store/api/annotation/ProtectedApiAnnotation.kt
+++ src/main/kotlin/com/talgat/store/api/annotation/ProtectedApiAnnotation.kt
@@ -2,13 +2,10 @@ package com.talgat.store.api.annotation
 
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
-//@Repeatable
 @RequestMapping("api/protected")
-@RestController
 @Secured("ROLE_ADMIN")
 annotation class ProtectedApiAnnotation
--- src/main/kotlin/com/talgat/store/api/annotation/PublicApiAnnotation.kt
+++ src/main/kotlin/com/talgat/store/api/annotation/PublicApiAnnotation.kt
@@ -1,12 +1,9 @@
 package com.talgat.store.api.annotation
 
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
-//@Inherited
 @RequestMapping("api/public")
-@RestController
 annotation class PublicApiAnnotation
