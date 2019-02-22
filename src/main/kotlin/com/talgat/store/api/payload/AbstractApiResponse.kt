7cb1740e47e5f855825eee68369619515f498ed0
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-02-22 16:07:20 +0600

    Payload for response remove property meessage

--- src/main/kotlin/com/talgat/store/api/payload/AbstractApiResponse.kt
+++ src/main/kotlin/com/talgat/store/api/payload/AbstractApiResponse.kt
@@ -2,7 +2,7 @@ package com.talgat.store.api.payload
 
 import org.springframework.http.HttpStatus
 
-abstract class AbstractApiResponse(val meessage: String) {
+abstract class AbstractApiResponse {
     val code = 200
     val status = HttpStatus.OK
 }
--- src/main/kotlin/com/talgat/store/api/payload/ItemResponse.kt
+++ src/main/kotlin/com/talgat/store/api/payload/ItemResponse.kt
@@ -2,4 +2,4 @@ package com.talgat.store.api.payload
 
 data class ItemResponse(
         val message: String,
-        val id: Long) : AbstractApiResponse(message)
+        val id: Long) : AbstractApiResponse()
