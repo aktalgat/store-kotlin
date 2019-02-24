0b86cfbdc646e10ac9ede5611da593c167d01083
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-02-24 09:16:11 +0100

    Category entity add blocked property

--- src/main/kotlin/com/talgat/store/data/model/Category.kt
+++ src/main/kotlin/com/talgat/store/data/model/Category.kt
@@ -2,7 +2,8 @@ package com.talgat.store.data.model
 
 data class Category(
         val name: String,
-        val id: Long = 0
+        val id: Long = 0,
+        val blocked: Boolean = false
 ) {
     constructor(name: String) : this(name, 0)
 }
