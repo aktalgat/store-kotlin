c4758064c4b9d5b077ee49953b44fd04f56df104
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-02-26 16:45:08 +0600

    Change admin initial phone

--- database/sql/initial-data/01-initial-data.sql
+++ database/sql/initial-data/01-initial-data.sql
@@ -4,7 +4,7 @@
 INSERT INTO core.users(id, phone, email, first_name, last_name, password, enabled) VALUES
   (1, '+996772136130', 'user@domain.com', 'Александр', 'Иванов', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', true);
 INSERT INTO core.users(id, phone, email, first_name, last_name, password, enabled) VALUES
-  (2, '+996557136130', 'admin@domain.com', 'Роман', 'Александров', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', true);
+  (2, '+996705136130', 'admin@domain.com', 'Роман', 'Александров', '$2a$10$h7MeivRcGNAnDS4p4buFuuYIbruyKcmT7DbKXwDagwNyMnvnuhXLS', true);
 
 INSERT INTO core.roles(id, name) VALUES (1, 'ROLE_USER');
 INSERT INTO core.roles(id, name) VALUES (2, 'ROLE_ADMIN');
